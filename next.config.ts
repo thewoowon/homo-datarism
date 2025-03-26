92d5303f033fc634360b46de90f6a6af19b9c7d5
thewoowon <thewoowon@naver.com>
2025-03-27 06:28:02 +0900

    feat: raw-loader config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
   /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }; // three.js 사용 시 필요할 수 있음
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs)$/,
+      use: "raw-loader",
+    });
+
+    config.resolve.extensions.push(".glsl", ".vs", ".fs");
+
+    return config;
+  },
 };
 
 export default nextConfig;
